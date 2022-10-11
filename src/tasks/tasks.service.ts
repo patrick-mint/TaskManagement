9692c89206965626801b0f6ec5f3e21f88560211
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-10-11 21:57:12 +0700

    Project Task Management by NestJS

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+}
