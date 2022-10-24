a2401888e6e593f0ce8cc9f6abd87f71c2bf2658
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-10-24 21:09:55 +0700

    update return status for not found task, update, delete

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -34,7 +34,18 @@ export class TasksService {
     }
 
     getTaskByID(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+
+        //try to get task
+        //if not found , throw an error 404 not found
+        //otherwise return found task
+
+        const found = this.tasks.find((task) => task.id === id);
+
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -51,7 +62,8 @@ export class TasksService {
     }
 
     deleteTask(id: string) : void {
-        this.tasks = this.tasks.filter((task) => task.id !==id)
+        const found = this.getTaskByID(id);
+        this.tasks = this.tasks.filter((task) => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus) {
